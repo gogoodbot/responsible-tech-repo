9f7ae06cb0cd301143a93a7e3c8fd456209e78c5
davique0 <davique@gmail.com>
2023-11-13 17:17:45 -0800

    Install ShadCN

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,23 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    // container: {
-    //   center: true,
-    //   padding: "2rem",
-    //   screens: {
-    //     "2xl": "1400px",
-    //   },
-    // },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    screens: {
+      xs: "475px",
+      sm: "768px",
+      md: "1060px",
+    },
     extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        openSans: ["Open Sans"],
+      },
       colors: {
         goodbot: {
           primary: {
@@ -24,23 +33,66 @@ module.exports = {
             skyBlue: "#06BADB",
             starryNightBlack: "#242424",
             offWhite: "#F1E7DD",
-            gray: "#f4f4f5",
+            gray: "#F5F5F5",
           },
           secondary: {
             neonGreen: "#96EA61",
             lightBlue: "#A3E2EC",
           },
         },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
-      fontFamily: {
-        poppins: ["Poppins"],
-        openSans: ["Open Sans"],
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
-    },
-    screens: {
-      xs: "475px",
-      sm: "768px",
-      md: "1060px",
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
