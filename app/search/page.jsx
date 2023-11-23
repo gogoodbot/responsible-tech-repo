2ec997b5d44f0cafd15066e660d392bd8e462cb4
Kabir Sewani <kabir.sewani@gmail.com>
2023-11-23 17:36:50 -0500

    updates to search page

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,11 +1,93 @@
+import Search from "../comps/Search";
 import Nav from "../comps/nav";
-
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function search() {
   return (
-    <section>
-      <Nav />
-      <h2>Search</h2>
+    <>
+    <Nav />
+
+    <section className="flex-1 space-y-4 p-8 pt-6">
+      <Search />
+
+      <div className="flex flex-wrap overflow-hidden">
+        <div className="w-1/3 px-1 my-1 sm:w-full sm:px-1 sm:my-1 md:w-1/3 md:px-1 md:my-1 lg:w-1/3 lg:w-1/3 lg:w-1/3 xl:w-1/3 ">
+          <Card>
+            <CardHeader>
+              <CardTitle>Digital Charter Implimentation Act</CardTitle>
+              <CardDescription>
+              (Bill C-27)
+                </CardDescription>
+              <CardDescription>
+                
+              <Badge variant="secondary">Legislation</Badge>
+              <Badge variant="secondary">Policy</Badge>
+              <Badge variant="secondary">tag</Badge>
+              </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum justo tristique, lacinia erat dapibus, placerat justo. Fusce facilisis arcu vitae eleifend imperdiet. Praesent metus velit, convallis a dapibus vulputate, accumsan a nisl. Mauris ut enim volutpat, tempor sapien ac, tempor ante.</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+        <div className="w-1/3 px-1 my-1 sm:w-full sm:px-1 sm:my-1 md:w-1/3 md:px-1 md:my-1 lg:w-1/3 lg:w-1/3 lg:w-1/3 xl:w-1/3 ">
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <Badge variant="secondary">tag</Badge>
+              <Badge variant="secondary">tag</Badge>
+              <Badge variant="secondary">tag</Badge>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+        <div className="w-1/3 px-1 my-1 sm:w-full sm:px-1 sm:my-1 md:w-1/3 md:px-1 md:my-1 lg:w-1/3 lg:w-1/3 lg:w-1/3 xl:w-1/3 ">
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <Badge variant="secondary">tag</Badge>
+              <Badge variant="secondary">tag</Badge>
+              <Badge variant="secondary">tag</Badge>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+        <div className="w-1/3 px-1 my-1 sm:w-full sm:px-1 sm:my-1 md:w-1/3 md:px-1 md:my-1 lg:w-1/3 lg:w-1/3 lg:w-1/3 xl:w-1/3 "></div>
+        <div className="w-1/3 px-1 my-1 sm:w-full sm:px-1 sm:my-1 md:w-1/3 md:px-1 md:my-1 lg:w-1/3 lg:w-1/3 lg:w-1/3 xl:w-1/3 "></div>
+        <div className="w-1/3 px-1 my-1 sm:w-full sm:px-1 sm:my-1 md:w-1/3 md:px-1 md:my-1 lg:w-1/3 lg:w-1/3 lg:w-1/3 xl:w-1/3 "></div>
+        <div className="w-1/3 px-1 my-1 sm:w-full sm:px-1 sm:my-1 md:w-1/3 md:px-1 md:my-1 lg:w-1/3 lg:w-1/3 lg:w-1/3 xl:w-1/3 "></div>
+        <div className="w-1/3 px-1 my-1 sm:w-full sm:px-1 sm:my-1 md:w-1/3 md:px-1 md:my-1 lg:w-1/3 lg:w-1/3 lg:w-1/3 xl:w-1/3 "></div>
+        <div className="w-1/3 px-1 my-1 sm:w-full sm:px-1 sm:my-1 md:w-1/3 md:px-1 md:my-1 lg:w-1/3 lg:w-1/3 lg:w-1/3 xl:w-1/3 "></div>
+        <div className="w-1/3 px-1 my-1 sm:w-full sm:px-1 sm:my-1 md:w-1/3 md:px-1 md:my-1 lg:w-1/3 lg:w-1/3 lg:w-1/3 xl:w-1/3 "></div>
+      </div>
     </section>
+    </>
   );
 }
