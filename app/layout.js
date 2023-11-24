b394d7f8b194aefdb9fc2c7f9c90c96848bdf6b9
davique0 <davique@gmail.com>
2023-11-24 10:30:38 -0800

    Add Supabase and test page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Nav from "./comps/nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <div className="absolute top-0 block w-screen">
+        <Nav />
+      </div>
+      <body className={`${inter.className} mt-16 pt-5`}>{children}</body>
     </html>
   );
 }
