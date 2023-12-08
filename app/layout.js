3b525d432f541b92c484fd41feef2d9e39731a75
kabir <kabir.sewani@gmail.com>
2023-12-08 12:42:55 -0500

    updated nav: +responsive +styling

--- app/layout.js
+++ app/layout.js
@@ -13,9 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} flex-1`}>
-        <div className="absolute top-0 block w-screen">
+        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="container flex h-14 items-center">
           <Navbar />
         </div>
+        </header>
+
         {children}
       </body>
     </html>
