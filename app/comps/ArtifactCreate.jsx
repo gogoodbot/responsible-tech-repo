2834b4a25a4490068820db9ee645054cfeba020a
Leventbk <leventbkarakus@hotmail.com>
2024-02-07 13:58:53 -0500

    Update ArtifactCreate component and static PolicyForm

--- app/comps/ArtifactCreate.jsx
+++ app/comps/ArtifactCreate.jsx
@@ -2,13 +2,15 @@
 "use client"
 import React from "react";
 // import ArtifactForm from "./ArtifactForm";
-import LitigationForm from "../create-artifact-page/LitigationForm";
+// import LitigationForm from "../create-artifact-page/LitigationForm";
+import PolicyForm from "../create-artifact-page/PolicyForm";
 
 export default function ArtifactCreate() {
   return (
     <div>
       {/* <ArtifactForm /> */}
-      <LitigationForm />
+      {/* <LitigationForm /> */}
+      <PolicyForm/>
     </div>
   );
 }
