c86fe9979e7da20f1695462e708a19e9fd33e0c2
Leventbk <leventbkarakus@hotmail.com>
2024-02-12 11:25:10 -0500

    add a mock test

---
+++ app/test/actions.test.js
@@ -0,0 +1,11 @@
+test('adds two positive numbers', () => {
+    // Arrange
+    const num1 = 5;
+    const num2 = 10;
+  
+    // Act
+    const result = num1 + num2;
+  
+    // Assert
+    expect(result).toBe(15);
+  });
