4d849a3401369b794fc0d3cc0cef7345ba519c75
kazvee <kazveedev@gmail.com>
2024-02-12 17:59:58 -0700

    Update hashtag icon used on pre-made query buttons

--- app/page.js
+++ app/page.js
@@ -1,17 +1,12 @@
-import Image from "next/image";
-import "./globals.css";
-import Navbar from "./comps/Navbar";
-import Hero from "./comps/Hero";
-import Search from "./comps/Search";
-import Results from "./results/page";
-import ResultCard from "./comps/ResultCard";
-import SearchTabs from "./comps/SearchTabs";
-import Tags from "./comps/Tags";
-import { Badge } from "../components/ui/badge";
-import { searchTags } from "@/lib/actions";
-import Link from "next/link";
-import { Separator } from "../components/ui/separator";
-import { Hash } from "lucide-react";
+import { searchTags } from '@/lib/actions';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Badge } from '../components/ui/badge';
+import { Separator } from '../components/ui/separator';
+import Hero from './comps/Hero';
+import Search from './comps/Search';
+import SearchTabs from './comps/SearchTabs';
+import './globals.css';
 
 export default async function Home() {
   const tags = await searchTags();
@@ -26,28 +21,34 @@ export default async function Home() {
             </div>
           </div>
           <div className="flex-1 space-y-4 p-8 pt-6">
-
-          {/* tags component */}
-          <div className="relative">
-        <div className="mb-4 flex items-center">
-          
-          {tags &&
-            tags.map((tag) => (
-              <Badge className="cursor-pointer mr-4 bg-goodbot-primary-blue text-white hover:text-white hover:bg-black" key={tag.keyword}>
-                <Hash className="mr-1" size={16}/>
-                <Link
-                  className="flex items-center"
-                  href={`/results?query=${tag.keyword}`}
-                >
-                  {tag.keyword}
-                </Link>
-              </Badge>
-            ))}
-        </div>
-      </div>
-      <Separator />
+            {/* tags component */}
+            <div className="relative">
+              <div className="mb-4 flex items-center">
+                {tags &&
+                  tags.map((tag) => (
+                    <Badge
+                      className="cursor-pointer mr-4 bg-goodbot-primary-blue text-white hover:text-white hover:bg-black"
+                      key={tag.keyword}
+                    >
+                      <Image
+                        src="https://img.icons8.com/nolan/64/ffffff/hashtag-key.png"
+                        alt="hashtag-key"
+                        width={32}
+                        height={32}
+                        className="mr-1"
+                      />
+                      <Link
+                        className="flex items-center"
+                        href={`/results?query=${tag.keyword}`}
+                      >
+                        {tag.keyword}
+                      </Link>
+                    </Badge>
+                  ))}
+              </div>
+            </div>
+            <Separator />
             <div className="flex items-center justify-between space-y-2">
-              
               <SearchTabs />
             </div>
           </div>
