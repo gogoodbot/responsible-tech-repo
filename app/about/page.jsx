532c4e01ea761f8cc011e88d8448f0c24eb5030e
kazvee <kazveedev@gmail.com>
2024-02-13 19:37:15 -0700

    Add basic content to About page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import Navbar from "../comps/Navbar";
 
-function about() {
+function About() {
   return (
-    <section>
-      <h2>About</h2>
+    <section className="text-center">
+      <h1 className="text-xl font-bold mt-10">
+        GoodBot - Responsible Tech Repo
+      </h1>
+      <p className="text-lg text-muted-foreground mt-8">
+        GoodBot strengthens technology governance. We&apos;re working towards building a more humane technology ecosystem.
+      </p>
     </section>
   );
 }
 
-export default about;
+export default About;
