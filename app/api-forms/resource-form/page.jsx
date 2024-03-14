eef62dd71ea3ed15e2487ae0ae1c2c61f3002426
Atena Razm <atenarazm@gmail.com>
2024-03-13 23:21:00 -0700

    ResourceForm created, it works, need final confirmation

--- app/api-forms/resource-form/page.jsx
+++ app/api-forms/resource-form/page.jsx
@@ -1,13 +1,19 @@
 'use client';
 
+import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import React, { useState } from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 const ResourceForm = () => {
   const [formData, setFormData] = useState({
     name: '',
-    focousArea: '',
+    focusArea: '',
     summary: '',
     link: '',
     post: '',
@@ -33,7 +39,7 @@ const ResourceForm = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className='flex w-full max-w-7xl items-center space-x-2 '
+      className='w-full max-w-7xl items-center space-y-2 '
     >
       <Input
         name='name'
@@ -43,14 +49,93 @@ const ResourceForm = () => {
         value={formData.name}
         className='px-4 border border-gray-300 rounded-md'
       />
-      {/* <Input
-        htmlFor='focusArea'
+      <Input
+        name='focusArea'
         type='text'
         placeholder='Focus Area'
         onChange={handleChange}
         value={formData.focusArea}
         className='px-4 border border-gray-300 rounded-md'
+      />
+      <Input
+        name='summary'
+        type='text'
+        placeholder='Summary'
+        onChange={handleChange}
+        value={formData.summary}
+        className='px-4 border border-gray-300 rounded-md'
+      />
+      <Input
+        name='link'
+        type='text'
+        placeholder='Link'
+        onChange={handleChange}
+        value={formData.link}
+        className='px-4 border border-gray-300 rounded-md'
+      />
+      <Input
+        name='post'
+        type='text'
+        placeholder='Post'
+        onChange={handleChange}
+        value={formData.post}
+        className='px-4 border border-gray-300 rounded-md'
+      />
+      <Input
+        name='notes'
+        type='text'
+        placeholder='Notes'
+        onChange={handleChange}
+        value={formData.notes}
+        className='px-4 border border-gray-300 rounded-md'
+      />
+      <Input
+        name='format'
+        type='text'
+        placeholder='Format'
+        onChange={handleChange}
+        value={formData.format}
+        className='px-4 border border-gray-300 rounded-md'
+      />
+      {/* <Input
+        name='tags'
+        type='text'
+        placeholder='Tags'
+        onChange={handleChange}
+        value={formData.tags}
+        className='px-4 border border-gray-300 rounded-md'
       /> */}
+      <div className='form-group space-y-2'>
+        <label htmlFor='tags' className='sr-only'>
+          Tags
+        </label>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='outline'>
+              <span style={{ color: 'gray' }}>Tag</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuItem
+              onClick={() => setFormData({ ...formData, tags: 'Tag-1' })}
+            >
+              Tag-1
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => setFormData({ ...formData, tags: 'Tag-2' })}
+            >
+              Tag-2
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => setFormData({ ...formData, tags: 'Tag-3' })}
+            >
+              Tag-3
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <p>{formData.tags}</p>{' '}
+        {/* this might need to a <ul> if there are supposed to be more than one tag picked */}
+      </div>
       <Button
         variant='ghost'
         className='outline-none cursor-pointer border-2 border-black rounded-md text-white bg-black px-5 py-3 text-center transition duration-150 ease-in-out hover:bg-goodbot-primary-blue hover:border-goodbot-primary-blue hover:text-whit  dark:bg-white dark:text-black dark:border-white dark:hover:bg-goodbot-primary-blue dark:hover:border-goodbot-primary-blue dark:hover:text-white'
