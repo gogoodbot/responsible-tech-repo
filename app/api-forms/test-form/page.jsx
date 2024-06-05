14170d0494e6a88938ebf75a6ab566c8fa264426
Atena Razm <atenarazm@gmail.com>
2024-06-05 10:46:26 -0700

    form title added

--- app/api-forms/test-form/page.jsx
+++ app/api-forms/test-form/page.jsx
@@ -58,186 +58,189 @@ const TestForm = () => {
 
   return (
     <div className='flex items-center justify-center min-h-screen p-4'>
-      <form
-        onSubmit={handleSubmit}
-        className='w-full max-w-7xl bg-white p-8 rounded-md shadow-md space-y-4'
-      >
-        <label className='pb-2 block text-lg text-gray-600'>
-          Name
-          <Input
-            name='name'
-            type='text'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={formData.name}
-            className={generalFieldClassName}
-            required
-          />
-          <ErrorMessage error={errors.name} />
-        </label>
-
-        <label className='pb-2 block text-lg text-gray-600'>
-          Focus Area
-          <Input
-            name='focusArea'
-            type='text'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={formData.focusArea}
-            className={generalFieldClassName}
-            required
-          />
-          <ErrorMessage error={errors.focusArea} />
-        </label>
-
-        <label className='pb-2 block text-lg text-gray-600'>
-          Summary
-          <div>
-            <textarea
-              name='summary'
+      <div className='w-full max-w-7xl bg-white p-8 rounded-md shadow-md'>
+        <h1 className='text-3xl font-bold my-8'>Test form</h1>
+        <form
+          onSubmit={handleSubmit}
+          className='w-full max-w-7xl bg-white p-8 rounded-md space-y-4'
+        >
+          <label className='pb-2 block text-lg text-gray-600'>
+            Name
+            <Input
+              name='name'
+              type='text'
               onChange={handleChange}
               onBlur={handleBlur}
-              value={formData.summary}
-              className={`${generalFieldClassName} h-40 resize-y`}
+              value={formData.name}
+              className={generalFieldClassName}
               required
             />
-          </div>
-          <ErrorMessage error={errors.summary} />
-        </label>
-
-        <label className='pb-2 block text-lg text-gray-600'>
-          Link
-          <Input
-            name='link'
-            type='url'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={formData.link}
-            className={generalFieldClassName}
-            required
-          />
-          <ErrorMessage error={errors.link} />
-        </label>
-
-        <label className='pb-2 block text-lg text-gray-600'>
-          Post
-          <div>
-            <textarea
-              name='post'
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={formData.posts}
-              className={`${generalFieldClassName} h-40 resize-y`}
-              required
-            />
-          </div>
-          <ErrorMessage error={errors.post} />
-        </label>
-
-        <label className='pb-2 block text-lg text-gray-600'>
-          Notes
-          <div>
-            <textarea
-              name='notes'
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={formData.notes}
-              className={`${generalFieldClassName} h-40 resize-y`}
-              required
-            />
-          </div>
-          <ErrorMessage error={errors.notes} />
-        </label>
-
-        <label className='pb-2 block text-lg text-gray-600'>
-          Format
-          <Input
-            name='format'
-            type='text'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={formData.format}
-            className={generalFieldClassName}
-            required
-          />
-          <ErrorMessage error={errors.format} />
-        </label>
-
-        <div className='form-group space-y-2'>
-          <label className='mb-2 block'>
-            Tags
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button className='ml-4' variant='outline'>
-                  <span style={{ color: 'gray' }}>Select Tags</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align='end'>
-                {Tags.map((tag) => (
-                  <DropdownMenuItem
-                    key={tag.id}
-                    onClick={() => handleTags(tag)}
-                  >
-                    {tag.name}
-                  </DropdownMenuItem>
-                ))}
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <ErrorMessage error={errors.name} />
           </label>
-          <ErrorMessage error={errors.tags} />
-        </div>
-        <div>
-          <ul>
-            {formData.tags.map((tag) => (
-              <li key={tag.id}>{tag.name}</li>
-            ))}
-          </ul>
-        </div>
 
-        <label className='pb-2 block text-lg text-gray-600'>
-          Username
-          <Input
-            name='username'
-            type='text'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={formData.username}
-            className={generalFieldClassName}
-            required
-          />
-          <ErrorMessage error={errors.username} />
-        </label>
+          <label className='pb-2 block text-lg text-gray-600'>
+            Focus Area
+            <Input
+              name='focusArea'
+              type='text'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={formData.focusArea}
+              className={generalFieldClassName}
+              required
+            />
+            <ErrorMessage error={errors.focusArea} />
+          </label>
 
-        <label className='pb-2 block text-lg text-gray-600'>
-          Password
-          <Input
-            name='password'
-            type='password'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={formData.password}
-            className={generalFieldClassName}
-            required
-          />
-          <ErrorMessage error={errors.password} />
-        </label>
+          <label className='pb-2 block text-lg text-gray-600'>
+            Summary
+            <div>
+              <textarea
+                name='summary'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={formData.summary}
+                className={`${generalFieldClassName} h-40 resize-y`}
+                required
+              />
+            </div>
+            <ErrorMessage error={errors.summary} />
+          </label>
 
-        <Button
-          variant='ghost'
-          className={`${generalButtonClassName} hover:bg-goodbot-primary-blue hover:border-goodbot-primary-blue hover:text-whit  dark:bg-white dark:text-black dark:border-white dark:hover:bg-goodbot-primary-blue dark:hover:border-goodbot-primary-blue dark:hover:text-white`}
-          type='submit'
-        >
-          Submit Form
-        </Button>
-        <Button
-          variant='ghost'
-          className={`${generalButtonClassName} ml-4 hover:bg-red-600 hover:border-red-600 hover:text-whit  dark:bg-white dark:text-black dark:border-white dark:hover:bg-red-600 dark:hover:border-red-600 dark:hover:text-white`}
-          type='button'
-          onClick={resetForm}
-        >
-          Reset Form
-        </Button>
-      </form>
+          <label className='pb-2 block text-lg text-gray-600'>
+            Link
+            <Input
+              name='link'
+              type='url'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={formData.link}
+              className={generalFieldClassName}
+              required
+            />
+            <ErrorMessage error={errors.link} />
+          </label>
+
+          <label className='pb-2 block text-lg text-gray-600'>
+            Post
+            <div>
+              <textarea
+                name='post'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={formData.posts}
+                className={`${generalFieldClassName} h-40 resize-y`}
+                required
+              />
+            </div>
+            <ErrorMessage error={errors.post} />
+          </label>
+
+          <label className='pb-2 block text-lg text-gray-600'>
+            Notes
+            <div>
+              <textarea
+                name='notes'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={formData.notes}
+                className={`${generalFieldClassName} h-40 resize-y`}
+                required
+              />
+            </div>
+            <ErrorMessage error={errors.notes} />
+          </label>
+
+          <label className='pb-2 block text-lg text-gray-600'>
+            Format
+            <Input
+              name='format'
+              type='text'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={formData.format}
+              className={generalFieldClassName}
+              required
+            />
+            <ErrorMessage error={errors.format} />
+          </label>
+
+          <div className='form-group space-y-2'>
+            <label className='mb-2 block'>
+              Tags
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button className='ml-4' variant='outline'>
+                    <span style={{ color: 'gray' }}>Select Tags</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align='end'>
+                  {Tags.map((tag) => (
+                    <DropdownMenuItem
+                      key={tag.id}
+                      onClick={() => handleTags(tag)}
+                    >
+                      {tag.name}
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </label>
+            <ErrorMessage error={errors.tags} />
+          </div>
+          <div>
+            <ul>
+              {formData.tags.map((tag) => (
+                <li key={tag.id}>{tag.name}</li>
+              ))}
+            </ul>
+          </div>
+
+          <label className='pb-2 block text-lg text-gray-600'>
+            Username
+            <Input
+              name='username'
+              type='text'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={formData.username}
+              className={generalFieldClassName}
+              required
+            />
+            <ErrorMessage error={errors.username} />
+          </label>
+
+          <label className='pb-2 block text-lg text-gray-600'>
+            Password
+            <Input
+              name='password'
+              type='password'
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={formData.password}
+              className={generalFieldClassName}
+              required
+            />
+            <ErrorMessage error={errors.password} />
+          </label>
+
+          <Button
+            variant='ghost'
+            className={`${generalButtonClassName} hover:bg-goodbot-primary-blue hover:border-goodbot-primary-blue hover:text-whit  dark:bg-white dark:text-black dark:border-white dark:hover:bg-goodbot-primary-blue dark:hover:border-goodbot-primary-blue dark:hover:text-white`}
+            type='submit'
+          >
+            Submit Form
+          </Button>
+          <Button
+            variant='ghost'
+            className={`${generalButtonClassName} ml-4 hover:bg-red-600 hover:border-red-600 hover:text-whit  dark:bg-white dark:text-black dark:border-white dark:hover:bg-red-600 dark:hover:border-red-600 dark:hover:text-white`}
+            type='button'
+            onClick={resetForm}
+          >
+            Reset Form
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
