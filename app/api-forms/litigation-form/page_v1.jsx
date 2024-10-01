91c432d89866f96a7a43b5d6235296a1cb1547fd
Atena Razm <atenarazm@gmail.com>
2024-10-01 11:52:30 -0700

    a copy of LitigationForm created to clean up

--- app/api-forms/litigation-form/page_v1.jsx
+++ app/api-forms/litigation-form/page_v1.jsx
@@ -6,14 +6,25 @@ import { Tags, REGEX_PATTERNS } from '../api-data';
 import useForm from '../useForm';
 import SelectCountry from '@/components/select-location/select-country';
 import { submitToLitigation } from '../submit-handler';
+// import SelectProvince from '@/components/select-location/select-province';
+// import SelectCity from '@/components/select-location/select-city';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@radix-ui/react-dropdown-menu';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { getLitigation } from './apiLitigation';
 import { useEffect, useState } from 'react';
+// import toast from 'react-hot-toast';
 
 const initialState = {
   created_by: '',
@@ -30,12 +41,37 @@ const initialState = {
   start_date: '', // TODO: startDate: '' >> TODO: fix it in database
   jurisdiction: '',
   tags: [], // TODO: Get them from API
+  // username: '',
+  // password: '',
 };
 
 const ErrorMessage = ({ error }) => {
   if (!error) return null;
   return <p className='text-red-500'>{error}</p>;
 };
+
+function LitigationTable() {
+  const {
+    isLoading,
+    error,
+    data: litigations,
+  } = useQuery({
+    queryKey: ['Litigation'],
+    queryFn: getLitigation,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) {
+    console.log(error);
+    throw new Error("Litigation data couln't be fetched");
+  }
+
+  // I can read from the supabase Litigation table
+  console.log(litigations);
+  initialState.name = litigations[0].name;
+
+  return <p>litigations</p>;
+}
 
 const LitigationForm = () => {
   const {
@@ -45,15 +81,37 @@ const LitigationForm = () => {
     handleChange,
     handleCountryChange,
     selectedCountry,
+    handleProvinceChange,
+    selectedProvince,
+    handleCityChange,
+    selectedCity,
     handleBlur,
     handleSubmit,
     handleTags,
     handleResetForm,
     generalFieldClassName,
     generalButtonClassName,
+    // handleDelete,
+    // isDeleting,
   } = useForm(initialState, REGEX_PATTERNS, submitToLitigation);
 
   const [isUpdate] = useState(false);
+  // const queryClient = useQueryClient();
+  // const { isLoading: isDeleting, mutate } = useMutation({
+  //   // mutationFn: (id) => deleteLitigation(id),
+  //   mutationFn: deleteLitigation,
+  //   onSuccess: () => {
+  //     toast.success('Litigation successfuly deleted');
+  //     queryClient.invalidateQueries({ queryKey: ['Litigation'] });
+  //   },
+  //   onError: (err) => toast.error(err.ErrorMessage),
+  // });
+
+  // function handleDelete() {
+  //   const testId = 'e6280033-52c7-4725-85f0-ee5b9d83591f';
+  //   mutate(testId);
+  //   console.log('id: ', testId, ' deleted');
+  // }
 
   useEffect(() => {
     if (!formData.litigation_id) {
@@ -65,11 +123,17 @@ const LitigationForm = () => {
   }, [formData.litigation_id, setFormData]);
 
   return (
+    // <QueryClientProvider client={queryClient}>
     <>
       <ReactQueryDevtools initialIsOpen={false} />
       <div className='flex items-center justify-center min-h-screen p-4'>
         <div className='w-full max-w-7xl bg-white p-8 rounded-md shadow-md'>
           <h1 className='text-3xl font-bold my-8'>Litigation Form</h1>
+          {/* <LitigationTable /> */}
+          {/* <button disabled={isDeleting} onClick={handleDelete}>
+            Delete row
+          </button> */}
+
           <form
             onSubmit={handleSubmit}
             className='w-full max-w-7xl bg-white p-8 rounded-md space-y-4'
@@ -82,7 +146,26 @@ const LitigationForm = () => {
               />
               <ErrorMessage error={errors.country} />
             </label>
-
+            {/* --- TEST CODE --- */}
+            {/* <label className='pb-2 block text-lg text-gray-600'>
+            Test Code (Province)
+            <SelectProvince
+              onProvinceSelect={handleProvinceChange}
+              selectedProvince={selectedProvince}
+              countryCode={selectedCountry?.value}
+            />
+            <ErrorMessage error={errors.state} /> 
+          </label>
+          <label className='pb-2 block text-lg text-gray-600'>
+            Test Code (City)
+            <SelectCity
+              onCitySelect={handleCityChange}
+              countryCode={selectedCountry?.value}
+              stateCode={selectedProvince?.value}
+              selectedCity={selectedCity}
+            />
+            <ErrorMessage error={errors.city} />
+          </label> */}
             {/* Hide these later */}
             <label className='pb-2 block text-lg text-gray-600'>
               Created by
@@ -96,8 +179,8 @@ const LitigationForm = () => {
                 className={generalFieldClassName}
                 required
               />
-            </label>
-
+              {/* <ErrorMessage error={errors.name} /> */}
+            </label>{' '}
             <label className='pb-2 block text-lg text-gray-600'>
               Created on
               <Input
@@ -115,7 +198,7 @@ const LitigationForm = () => {
                 required
               />
             </label>
-
+            {/* <ErrorMessage error={errors.name} /> */}
             <label className='pb-2 block text-lg text-gray-600'>
               Litigation id
               <Input
@@ -287,7 +370,31 @@ const LitigationForm = () => {
                 ))}
               </ul>
             </div>
-
+            {/* <label className='pb-2 block text-lg text-gray-600'>
+              Username
+              <Input
+                name='username'
+                type='text'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={formData.username}
+                className={generalFieldClassName}
+                required
+              />
+              <ErrorMessage error={errors.username} />
+            </label> */}
+            {/* <label className='pb-2 block text-lg text-gray-600'>
+              Password
+              <Input
+                name='password'
+                type='password'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={formData.password}
+                className={generalFieldClassName}
+                required
+              />
+            </label> */}
             <Button
               variant='ghost'
               className={`${generalButtonClassName} hover:bg-goodbot-primary-blue hover:border-goodbot-primary-blue hover:text-whit  dark:bg-white dark:text-black dark:border-white dark:hover:bg-goodbot-primary-blue dark:hover:border-goodbot-primary-blue dark:hover:text-white`}
@@ -306,6 +413,7 @@ const LitigationForm = () => {
           </form>
         </div>
       </div>
+      {/* // </QueryClientProvider> */}
     </>
   );
 };
