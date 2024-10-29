daa206acedbf92414c8b19964823ff29151bf062
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2024-10-28 18:33:56 -0700

    Filter UI created

---
+++ app/comps/FilterDropdown.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { SlidersHorizontal as FilterIcon, ChevronsUp } from 'lucide-react';
+
+const filterData = [
+    {
+        title: "CONTENT TYPE",
+        type: "checkbox",
+        options: ['Magazine Article', 'Web Resource', 'Conference Proceeding', 'Journal Article', 'Newspaper Article']
+    },
+    {
+        title: "Discipline",
+        type: "checkbox",
+        options: ['business', 'comp science', 'engineering', 'government', 'law']
+    },
+    {
+        title: "SUBJECT TERMS",
+        type: "checkbox",
+        options: ['artificial intelligence', 'privacy', 'data integrity', 'bills', 'personal information']
+    },
+    {
+        title: "PUBLICATION DATE",
+        type: "radio",
+        options: ['1 year', '3 year', '5 year', '10 year', 'Custom']
+    },
+]
+
+const FilterDropdown = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="relative inline-block text-left">
+            <button
+                onClick={(e) => {
+                    e.preventDefault();
+                    toggleDropdown();
+                }}
+                className="shadow text-base font-bold text-sky-900 uppercase flex items-center justify-center gap-2 px-2 py-2.5 rounded-md border border-sky-900 min-w-[116px]"
+            >
+                <FilterIcon size={20} />
+                Filter
+            </button>
+
+            {isOpen && (
+                <div className="flex items-start absolute right-0 mt-2  bg-white border border-gray-200 rounded-lg shadow-lg p-4 z-10">
+                    {filterData.map((filterCategory, index) => (
+                        <div key={index} className="w-[227px]">
+                            <h3 className="font-semibold">{filterCategory.title}</h3>
+                            {filterCategory.options.map((option, i) => (
+                                filterCategory.type === 'checkbox' ? (
+                                    <label key={i} className="flex gap-4 items-center mt-2">
+                                        <input type="checkbox" className="accent-black" />
+                                        {option}
+                                    </label>
+                                ) : (
+                                    <label key={i} className="flex gap-4 items-center mt-2">
+                                        <input type="radio" name="date" className="accent-black" />
+                                        {option}
+                                    </label>
+                                )
+                            ))}
+                        </div>
+                    ))}
+
+
+                    <ChevronsUp size={20} onClick={toggleDropdown} className="cursor-pointer" />
+
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default FilterDropdown;
--- app/comps/Search.jsx
+++ app/comps/Search.jsx
@@ -2,16 +2,16 @@
 
 import React from "react";
 import { Search as SearchIcon } from "lucide-react";
-import { SlidersHorizontal as FilterIcon } from 'lucide-react';
+import FilterDropdown from "./FilterDropdown";
 
 
 
 const Search = () => {
   return (
     <form className="flex w-full gap-2">
-      <input type="text" placeholder="Search Goodbot ex. Digital Charter Canada" className="w-full border border-[#A1A1A1] rounded-md px-3 py-1.5 text-sm" />
-      <button className="text-base font-bold uppercase flex items-center justify-center gap-2 bg-[#A3E2EC] px-2 py-2.5 rounded-md min-w-[116px]"><SearchIcon size={20} />search</button>
-      <button className="text-base font-bold text-sky-900 uppercase flex items-center justify-center gap-2 px-2 py-2.5 rounded-md border border-sky-900 min-w-[116px]"><FilterIcon size={20} />filter</button>
+      <input type="text" placeholder="Search Goodbot ex. Digital Charter Canada" className="w-full shadow-sm border border-[#A1A1A1] rounded-md px-3 py-1.5 text-sm" />
+      <button className="shadow text-base font-bold uppercase flex items-center justify-center gap-2 bg-[#A3E2EC] px-2 py-2.5 rounded-md min-w-[116px]"><SearchIcon size={20} />search</button>
+      <FilterDropdown />
     </form>
   );
 };
