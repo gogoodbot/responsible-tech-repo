3bfb51052195eb5ce1d5ee3512b820f482edf030
Atena Razm <atenarazm@gmail.com>
2024-11-04 13:20:22 -0800

    Modal is a compond component 2

--- app/artifact/AddArtifact.jsx
+++ app/artifact/AddArtifact.jsx
@@ -17,25 +17,4 @@ function AddArtifact({ toRender }) {
   );
 }
 
-// function AddArtifact({ toRender }) {
-//   const [isModalOpen, setModalOpen] = useState(false); // Modal visibility state
-
-//   return (
-//     <>
-//       <button
-//         className='font-poppins font-bold text-1xl cursor-pointer px-3 py-2 my-3 rounded-sm overflow-hidden border bg-goodbot-primary-blue dark:bg-transparent shadow'
-//         onClick={() => setModalOpen((isModalOpen) => !isModalOpen)}
-//       >
-//         Create a new Litigation
-//       </button>
-//       {isModalOpen && (
-//         <Modal show={isModalOpen} onClose={() => setModalOpen(false)}>
-//           <h2>Create a New Litigation</h2>
-//           {toRender}
-//         </Modal>
-//       )}
-//     </>
-//   );
-// }
-
 export default AddArtifact;
