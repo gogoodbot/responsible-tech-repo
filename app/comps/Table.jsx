e9c2ac85af5853b5bff492bfa8ccf1edb0b8fb75
Atena Razm <atenarazm@gmail.com>
2024-11-07 09:30:13 -0800

    content table is scrolable vertically and horizontally

--- app/comps/Table.jsx
+++ app/comps/Table.jsx
@@ -10,31 +10,33 @@ const Table = ({
   resourceName,
 }) => {
   return (
-    <table className='min-w-full bg-white border border-gray-200'>
-      <thead>
-        <tr>
-          {headers.map((header, index) => (
-            <th key={index} className='py-2 px-4 border-b border-gray-200'>
-              {header}
-            </th>
+    <div className='overflow-auto max-h-[65vh]'>
+      <table className='min-w-full bg-white border border-gray-200'>
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index} className='py-2 px-4 border-b border-gray-200'>
+                {header}
+              </th>
+            ))}
+            <th className='py-2 px-4 border-b border-gray-200'></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <TableRow
+              key={index}
+              item={item}
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
+              toRender={toRender}
+              resourceName={resourceName}
+              headers={headers}
+            />
           ))}
-          <th className='py-2 px-4 border-b border-gray-200'></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <TableRow
-            key={index}
-            item={item}
-            handleEdit={handleEdit}
-            handleDelete={handleDelete}
-            toRender={toRender}
-            resourceName={resourceName}
-            headers={headers}
-          />
-        ))}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
