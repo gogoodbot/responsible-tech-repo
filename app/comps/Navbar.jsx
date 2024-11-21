f6fe969ac4e747e4c9874995e73994866672c4d2
Atena Razm <atenarazm@gmail.com>
2024-11-21 11:06:34 -0800

    prevent rendering the curren link click

--- app/comps/Navbar.jsx
+++ app/comps/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
 import { ModeToggle } from '@/components/mode-toggle';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+  const pathname = usePathname();
+
+  const NavLink = ({ href, children }) => {
+    const isActive = pathname === href;
+
+    return isActive ? (
+      <span className='ml-6 flex items-center space-x-2 text-gray-500 cursor-not-allowed'>
+        {children}
+      </span>
+    ) : (
+      <Link href={href} className='ml-6 flex items-center space-x-2'>
+        {children}
+      </Link>
+    );
+  };
+
   return (
     <div className='container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0'>
       <div className='logo'>
@@ -11,15 +28,10 @@ const Navbar = () => {
         </Link>
       </div>
       <nav className='flex items-center space-x-6 text-sm font-medium'>
-        <Link href='/login' className='ml-6 flex items-center space-x-2'>
-          Login
-        </Link>
-        <Link href='/dashboard' className='ml-6 flex items-center space-x-2'>
-          Dashboard
-        </Link>
-        <Link href='/about' className='ml-6 flex items-center space-x-2'>
-          About
-        </Link>
+        <NavLink href='/login'>Login</NavLink>
+        <NavLink href='/dashboard'>Dashboard</NavLink>
+        <NavLink href='/about'>About</NavLink>
+
         <ModeToggle />
       </nav>
     </div>
