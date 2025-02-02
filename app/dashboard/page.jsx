eb0460af3fb1f262bd832a8e5c957ef1872c793f
Atena Razm <atenarazm@gmail.com>
2025-02-01 22:49:57 -0800

    Add Artifact added to dashboard pages, code clean up done

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -6,7 +6,9 @@ import Policy from '../policy/page';
 import Organization from '../organization/page';
 import Resource from '../resource/page';
 import Stakeholder from '../stakeholder/page';
-// import AddArtifact from '../artifact/AddArtifact';
+import AddArtifact from '../../components/ui/add-artifact';
+
+// TO DO: create these forms and render on Add New ... artifact
 // import LitigationForm from '../api-forms/litigation-form/page';
 // import PolicyForm from '../api-forms/policy-form/page';
 // import OrganizationFrom from '../api-forms/organization-form/page';
@@ -31,22 +33,6 @@ function Dashboard() {
         : ''
     }`;
 
-  // const renderSection = () => {
-  //   switch (activeSection) {
-  //     case 'Litigation':
-  //       return <Litigation toRender={renderForm()} resourceName='litigation' />;
-  //     case 'Policy':
-  //       return <Policy toRender={renderForm()} resourceName='policy' />;
-  //     case 'Organization':
-  //       return <Organization />;
-  //     case 'Resource':
-  //       return <Resource />;
-  //     case 'Stakeholder':
-  //       return <Stakeholder />;
-  //     default:
-  //       return <Litigation />;
-  //   }
-  // };
   const renderSection = () => {
     switch (activeSection) {
       case 'Litigation':
@@ -64,22 +50,28 @@ function Dashboard() {
     }
   };
 
-  // const renderForm = () => {
-  //   switch (activeSection) {
-  //     case 'Litigation':
-  //       return <LitigationForm />;
-  //     case 'Policy':
-  //       return <PolicyForm />;
-  //     case 'Organization':
-  //       return <OrganizationFrom />;
-  //     case 'Resource':
-  //       return <ResourceForm />;
-  //     case 'Stakeholder':
-  //       return <StakeholderForm />;
-  //     default:
-  //       return <LitigationForm />;
-  //   }
-  // };
+  const renderForm = () => {
+    switch (activeSection) {
+      case 'Litigation':
+        return <p>Render litigation form here</p>;
+      // return <LitigationForm />;
+      case 'Policy':
+        return <p>Render policy form here</p>;
+      //   return <PolicyForm />;
+      case 'Organization':
+        return <p>Render organization form here</p>;
+      //   return <OrganizationFrom />;
+      case 'Resource':
+        return <p>Render resource form here</p>;
+      //   return <ResourceForm />;
+      case 'Stakeholder':
+        return <p>Render stakeholder form here</p>;
+      //   return <StakeholderForm />;
+      default:
+        return <p>Render litigation form here</p>;
+      //   return <LitigationForm />;
+    }
+  };
 
   const NavSection = () => {
     return (
@@ -105,7 +97,7 @@ function Dashboard() {
     return (
       <section className='overflow-hidden rounded-lg border bg-background dark:bg-transparent shadow bg-slate-50 px-3 py-1.5 '>
         {renderSection()}
-        {/* <AddArtifact toRender={toRender} activeSection={activeSection} /> */}
+        <AddArtifact toRender={toRender} activeSection={activeSection} />
       </section>
     );
   };
@@ -113,8 +105,7 @@ function Dashboard() {
   return (
     <div className='container relative mt-8'>
       <NavSection />
-      {/* <ContentSection toRender={renderForm()} /> */}
-      <ContentSection />
+      <ContentSection toRender={renderForm()} />
     </div>
   );
 }
