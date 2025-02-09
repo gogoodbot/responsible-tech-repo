3345adbd2e62c44b9af0e866f5810242df8a7772
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2025-02-09 14:39:58 -0800

    Removed theme

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
-import { ThemeProvider } from "@/components/theme-provider";
 import Footer from "../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,19 +17,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-screen bg-background font-sans antialiased`}>
-        {/* ThemeProvider+ DarkMode */}
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem={true}
-          disableTransitionOnChange
-        >
-          <div className="relative flex min-h-screen flex-col">
-            <Header />
-            <div className="flex-1">{children}</div>
-            <Footer/>
-          </div>
-        </ThemeProvider>
+        <div className="relative flex min-h-screen flex-col">
+          <Header />
+          <div className="flex-1">{children}</div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
