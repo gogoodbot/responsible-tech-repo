202dc17737642fc9b338ab2ac619411b5a1aca83
Gopikalakshmi <gopikalakshmia@gmail.com>
2025-07-15 20:53:10 -0400

    Added Modal and css

--- app/comps/Modal/Modal.jsx
+++ app/comps/Modal/Modal.jsx
@@ -5,23 +5,46 @@ const Modal = ({ isOpen, onClose, data }) => {
   return (
     <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center">
       <div className="modal-container">
+        <div className="close-select">     
+        </div>
+        <div className="close-button-div">
+            <button className="close-button" onClick={onClose}>
+              x
+            </button>
+          </div>
         <div className="modal-head">
-          {data?.logo && (
-            <div src="" alt="logo">
-              <img></img>
+          <div className="modal-head-title-wrapper">
+            <div className="modal-head-logo">
+              {data?.logo && <img src="" alt="logo"></img>}
             </div>
-          )}
-          {data?.name && <div className="head-title">{data.name}</div>}
-          {data?.type && <div className="head-type"><div className="head-typedesc">{data.type}</div></div>}
+            <div className="modal-head-title-div">
+              {data?.name && (
+                <div className="modal-head-title">{data.name}</div>
+              )}
+            </div>
+            <div className="modal-head-badge">
+              {data?.type && (
+                <div className="modal-head-badge-text">{data.type}</div>
+              )}
+            </div>
+          </div>
         </div>
-        <div className="about">
-          <div className="about-title">About</div>
-          {data?.summary && <div className="about-desc">{data.summary}</div>}
+        <div className="about-container">
+          <div className="about-heading">About</div>
+          <div className="about-desc">{data?.summary && data.summary}</div>
+          <div className="about-email">{data?.email && data.email}</div>
+          <div className="about-social">
+            <div className="about-social-linkedIn">
+              <div className="linkedin-Icon-wrapper">
+                <div className="linkedin-Icon-wrapper oval">
+              <div className="linkedin-shape oval">.</div>
+              <div className="linkedin-shape shape2">|</div>
+                 <div className="linkedin-shape shape1">n</div>
+                 </div>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <button className="close-button" onClick={onClose}>
-          X
-        </button>
       </div>
     </div>
   );
