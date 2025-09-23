f847bd236ae2750925796c3d1fa87ce15c349fe8
Gopikalakshmi <gopikalakshmia@gmail.com>
2025-09-22 22:26:43 -0400

    Added intial logo

--- app/comps/Modal/Modal.jsx
+++ app/comps/Modal/Modal.jsx
@@ -27,7 +27,7 @@ const Modal = ({ isOpen, onClose, data, isTopVoice }) => {
       };
   const splitName = ModalData?.name?.trim().split(/\s+/); // split on one or more spaces
   const initials = splitName?.map((c) => c[0]);
-  const logoIntials = initials?.join('');
+  const logoIntials = initials?.join("");
   if (!isOpen) return null;
   return (
     <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center">
@@ -45,14 +45,18 @@ const Modal = ({ isOpen, onClose, data, isTopVoice }) => {
           {ModalData?.logo && (
             <img src="image.png" alt="Logo" className="w-16 h-16" />
           )}
-          {isTopVoice && !ModalData.logo && <div className="font-bold bg-gray-300 rounded-full p-4"><p className="font-large font-bold">{logoIntials}</p></div>}
+          {isTopVoice && !ModalData.logo && (
+            <div className="font-bold bg-gray-300 rounded-full p-4">
+              <p className="font-large font-bold">{logoIntials}</p>
+            </div>
+          )}
           <div className="flex flex-col">
             {ModalData?.name && (
               <div className="text-xl font-bold tracking-wide text-slate-900 ">
                 {ModalData.name}
               </div>
             )}
-            <div className="w-[186px] h-6 font-poppins font-bold text-base leading-6 flex items-center tracking-[0.5px] uppercase text-[#020817] order-1 flex-none grow-0 whitespace-nowrap">
+            <div className="font-poppins font-bold text-base leading-6 flex items-center tracking-[0.5px] uppercase text-[#020817] order-1 flex-none grow-0 whitespace-wrap">
               {ModalData.title}
             </div>
           </div>
@@ -84,69 +88,21 @@ const Modal = ({ isOpen, onClose, data, isTopVoice }) => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <div className="w-full h-full rounded-full border-2 border-black bg-white opacity-25 flex items-center justify-center">
+                  <div className="w-full h-full rounded-full border-2 border-gray-300 bg-white  flex items-center justify-center">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="14"
                       height="14"
                       viewBox="0 0 24 24"
-                      fill="#000000"
+                      fill="black"
                     >
-                      <path d="M4.98 3.5C4.98 4.88 3.86 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM0 8h5v16H0V8zm7.5 0h4.7v2.3h.1c.65-1.2 2.2-2.5 4.5-2.5 4.8 0 5.7 3.1 5.7 7.1V24h-5v-7.6c0-1.8 0-4-2.4-4s-2.7 1.8-2.7 3.8V24h-5V8z" />
+                      <path
+                        fill="black"
+                        d="M4.98 3.5C4.98 4.88 3.86 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM0 8h5v16H0V8zm7.5 0h4.7v2.3h.1c.65-1.2 2.2-2.5 4.5-2.5 4.8 0 5.7 3.1 5.7 7.1V24h-5v-7.6c0-1.8 0-4-2.4-4s-2.7 1.8-2.7 3.8V24h-5V8z"
+                      />
                     </svg>
                   </div>
                 </a>
-              </div>
-            )}
-            {/* Instagram */}
-            {ModalData.instagram != null && (
-              <div className="w-11 h-11 relative">
-                <div className="w-full h-full border-2 border-black rounded-full bg-white opacity-25 flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="#242424"
-                  >
-                    <path d="M7.75 2h8.5A5.76 5.76 0 0 1 22 7.75v8.5A5.76 5.76 0 0 1 16.25 22h-8.5A5.76 5.76 0 0 1 2 16.25v-8.5A5.76 5.76 0 0 1 7.75 2Zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5a4.25 4.25 0 0 0 4.25-4.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5Zm4.25 4.25A4.25 4.25 0 1 1 7.75 12 4.25 4.25 0 0 1 12 7.75Zm0 1.5A2.75 2.75 0 1 0 14.75 12 2.75 2.75 0 0 0 12 9.25Zm4.75-2.25a1 1 0 1 1-1 1 1 1 0 0 1 1-1Z" />
-                  </svg>
-                </div>
-              </div>
-            )}
-
-            {/* Facebook */}
-            {ModalData.facebook != null && (
-              <div className="w-11 h-11 relative">
-                <div className="w-full h-full border-2 border-black rounded-full bg-white opacity-25 flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="14"
-                    height="14"
-                    viewBox="0 0 24 24"
-                    fill="#242424"
-                  >
-                    <path d="M22 12.07C22 6.48 17.52 2 12 2S2 6.48 2 12.07c0 4.84 3.44 8.85 7.94 9.8v-6.93H7.1v-2.87h2.84V9.41c0-2.8 1.66-4.35 4.2-4.35 1.22 0 2.5.22 2.5.22v2.75h-1.41c-1.39 0-1.82.87-1.82 1.76v2.11h3.1l-.5 2.87h-2.6v6.93c4.5-.95 7.94-4.96 7.94-9.8Z" />
-                  </svg>
-                </div>
-              </div>
-            )}
-
-            {/* YouTube */}
-            {ModalData.youtube != null && (
-              <div className="w-11 h-11 relative">
-                <div className="w-full h-full border-2 border-black rounded-full bg-white opacity-25 flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    fill="#242424"
-                  >
-                    <path d="M10 15.5l6-3.5-6-3.5v7z" />
-                    <path d="M21.8 8s-.2-1.4-.8-2c-.7-.8-1.5-.8-1.9-.9-2.7-.2-6.7-.2-6.7-.2h-.1s-4 0-6.7.2c-.4 0-1.2.1-1.9.9-.6.6-.8 2-.8 2S3 9.6 3 11.2v1.6c0 1.6.2 3.2.2 3.2s.2 1.4.8 2c.7.8 1.6.7 2 .8 1.5.1 6.5.2 6.5.2s4 0 6.7-.2c.4 0 1.2-.1 1.9-.9.6-.6.8-2 .8-2s.2-1.6.2-3.2v-1.6c0-1.6-.2-3.2-.2-3.2z" />
-                  </svg>
-                </div>
               </div>
             )}
             {/* Website */}
@@ -180,6 +136,66 @@ const Modal = ({ isOpen, onClose, data, isTopVoice }) => {
             )}
           </div>
         </div>
+        {/* Instagram */}
+        {ModalData.instagram != null && (
+          <div className="w-11 h-11 relative">
+            <div className="w-full h-full border-2  border-gray-300 rounded-full bg-white  flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="#242424"
+              >
+                <path
+                  fill="black"
+                  d="M7.75 2h8.5A5.76 5.76 0 0 1 22 7.75v8.5A5.76 5.76 0 0 1 16.25 22h-8.5A5.76 5.76 0 0 1 2 16.25v-8.5A5.76 5.76 0 0 1 7.75 2Zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5a4.25 4.25 0 0 0 4.25-4.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5Zm4.25 4.25A4.25 4.25 0 1 1 7.75 12 4.25 4.25 0 0 1 12 7.75Zm0 1.5A2.75 2.75 0 1 0 14.75 12 2.75 2.75 0 0 0 12 9.25Zm4.75-2.25a1 1 0 1 1-1 1 1 1 0 0 1 1-1Z"
+                />
+              </svg>
+            </div>
+          </div>
+        )}
+
+        {/* Facebook */}
+        {ModalData.facebook != null && (
+          <div className="w-11 h-11 relative">
+            <div className="w-full h-full border-2  border-gray-300 rounded-full bg-white flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="#242424"
+              >
+                <path
+                  fill="black"
+                  d="M22 12.07C22 6.48 17.52 2 12 2S2 6.48 2 12.07c0 4.84 3.44 8.85 7.94 9.8v-6.93H7.1v-2.87h2.84V9.41c0-2.8 1.66-4.35 4.2-4.35 1.22 0 2.5.22 2.5.22v2.75h-1.41c-1.39 0-1.82.87-1.82 1.76v2.11h3.1l-.5 2.87h-2.6v6.93c4.5-.95 7.94-4.96 7.94-9.8Z"
+                />
+              </svg>
+            </div>
+          </div>
+        )}
+
+        {/* YouTube */}
+        {ModalData.youtube != null && (
+          <div className="w-11 h-11 relative">
+            <div className="w-full h-full border-2  border-gray-300 rounded-full bg-white  flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="#242424"
+              >
+                <path fill="black" d="M10 15.5l6-3.5-6-3.5v7z" />
+                <path
+                  fill="black"
+                  d="M21.8 8s-.2-1.4-.8-2c-.7-.8-1.5-.8-1.9-.9-2.7-.2-6.7-.2-6.7-.2h-.1s-4 0-6.7.2c-.4 0-1.2.1-1.9.9-.6.6-.8 2-.8 2S3 9.6 3 11.2v1.6c0 1.6.2 3.2.2 3.2s.2 1.4.8 2c.7.8 1.6.7 2 .8 1.5.1 6.5.2 6.5.2s4 0 6.7-.2c.4 0 1.2-.1 1.9-.9.6-.6.8-2 .8-2s.2-1.6.2-3.2v-1.6c0-1.6-.2-3.2-.2-3.2z"
+                />
+              </svg>
+            </div>
+          </div>
+        )}
 
         {(data.publication || data.recommendation) && (
           <div className="px-8 flex flex-col gap-4">
