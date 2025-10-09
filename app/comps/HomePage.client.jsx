f936e23462af2f28d8f00e2b27221a4a2cec5cb9
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2025-10-08 19:49:47 -0700

    printing API key

--- app/comps/HomePage.client.jsx
+++ app/comps/HomePage.client.jsx
@@ -53,7 +53,7 @@ const HomePageClient = () => {
         setNonProfitsData(selected.nonprofits)
         setTopVoicesData(selected.experts)
     }
-
+    console.log(process.env.NEXT_PUBLIC_GOODBOT_API_URL)
 
 
     return (
