57d19687581868b673de7236e5d047fa9ee1eef2
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2025-10-15 19:38:25 -0700

    display logo for organizations

--- components/home-page-ui/OrganizationCard.jsx
+++ components/home-page-ui/OrganizationCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Image from "next/image";
 
 const OrganizationCard = ({ organization }) => {
     return (
         <div className="flex flex-col gap-2 bg-goodbot-background-light w-full p-4 rounded-lg border border-goodbot-gray-medium">
             <div className="flex items-start justify-between  w-full ">
-                <div className="text-sm h-10 px-9 content-center bg-goodbot-gray-light">Logo</div>
+                <Image src={organization.image_url} alt={`${organization.name}'s logo`} height={500} width={500} className="h-10 w-fit" />
             </div>
             <div className="flex flex-col gap-2 text-goodbot-text-dark">
                 <p className="text-sm font-bold">{organization.name} </p>
