027cde2a3869d19d3d00271ed4ca47589bd5b89c
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2025-12-09 15:59:39 -0800

    image placeholder size got fixed

--- app/comps/SearchCard.jsx
+++ app/comps/SearchCard.jsx
@@ -17,9 +17,9 @@ export default function SearchCard({ data, type }) {
     const getInitials = (first = "", last = "") =>
         `${first.charAt(0).toUpperCase() || ""}${last.charAt(0).toUpperCase() || ""}`;
     return (
-        <div className="flex justify-start items-center gap-8 px-6 py-8 border border-zinc-300 rounded">
+        <div className="flex justify-start items-stretch gap-8 px-6 py-8 border border-zinc-300 rounded">
             {type === 'expert' ? (
-                <div className="w-[104px] h-full self-stretch bg-goodbot-primary flex-shrink-0 text-2xl text-white flex justify-center items-center">{getInitials(data.first_name, data.last_name)}</div>
+                <div className="w-[104px] bg-goodbot-primary flex-shrink-0 text-2xl text-white flex justify-center items-center">{getInitials(data.first_name, data.last_name)}</div>
             ) : (<div className="w-[104px] h-auto bg-black flex-shrink-0"></div>)}
 
             <div className="flex flex-col gap-4 justify-center items-start">
